43a5ceec3fa90c1fdc9b2256f3fd65aeb66d6ecf
Marcos Vinicius <marcosvinicius.udia1256@gmail.com>
2024-01-24 15:37:57 -0300

    ✨  add new topics

--- app.vue
+++ app.vue
@@ -9,5 +9,6 @@
 <script lang="ts" setup>
 //todo: fazer o dark mode
 //todo: fazer o login
+//todo: fazer o cadasto de produtos
 //todo: configurar Store com Plinia para cada cadastro que for realizado
 </script>
